1bccdcce91cb27758bdd4d00067966f75a5e8e54
hengyangji <hengyangji@tencent.com>
2022-10-26 18:48:14 +0800

    简化二级页面

--- app/src/main/java/com/sun/sunpurecompose/SecondActivity.kt
+++ app/src/main/java/com/sun/sunpurecompose/SecondActivity.kt
@@ -3,28 +3,16 @@ package com.sun.sunpurecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.sun.sunpurecompose.ui.theme.SunPureComposeTheme
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         log("SecondActivity-onCreate")
         setContent {
-            SunPureComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Android")
         }
-        SunMeta().doSomeThing()
     }
 
     override fun onPause() {
